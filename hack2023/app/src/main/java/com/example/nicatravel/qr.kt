bb2af1664b832b0f10fa3e28c5b247a7d8aab67d
Waskar <waskareliasrios11@gmail.com>
2023-10-01 11:04:43 -0600

    Escaner QR Funcionando

--- hack2023/app/src/main/java/com/example/nicatravel/qr.kt
+++ hack2023/app/src/main/java/com/example/nicatravel/qr.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.example.nicatravel.databinding.ActivityQrBinding
 import com.google.zxing.integration.android.IntentIntegrator
 
@@ -19,6 +20,23 @@ class qr : AppCompatActivity() {
         binding.btnScanner.setOnClickListener { initScanner() }
     }
     private fun initScanner(){
-        IntentIntegrator(this).initiateScan()
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("Escanea Un codigo QR")
+        integrator.setTorchEnabled(true)
+        integrator.setBeepEnabled(true)
+        integrator.initiateScan()
     }
-}
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "El valor escaneado es: " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+    }
